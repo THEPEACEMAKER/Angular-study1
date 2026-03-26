e2d6fc23f9af613334d5122204045784c25c5541
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-03-26 19:29:36 +0200

    Add Invoice-Header TS code

--- src/app/Features/Invoices/Components/invoice-header/invoice-header.component.ts
+++ src/app/Features/Invoices/Components/invoice-header/invoice-header.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { Supplier } from '../../Models/product.model';
 
 @Component({
   selector: 'app-invoice-header',
-  imports: [],
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule
+  ],
   templateUrl: './invoice-header.component.html',
-  styleUrl: './invoice-header.component.css'
+  styleUrl: './invoice-header.component.scss'
 })
 export class InvoiceHeaderComponent {
-
+  @Input() headerForm!: FormGroup;
+  @Input() suppliers: Supplier[] = [];
 }
